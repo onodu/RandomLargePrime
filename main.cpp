a9c49ad89926c368c978aecaa3d3d7f0460e98b4
Ivan Bogdanov <c3t3o7@gmail.com>
2020-02-26 19:42:44 +0400

    Add parallel algorithm

--- main.cpp
+++ main.cpp
@@ -1,22 +1,64 @@
 #include <algorithm>
+#include <atomic>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/miller_rabin.hpp>
 #include <boost/random.hpp>
 #include <boost/random/random_device.hpp>
-#include <cmath>
+#include <functional>
 #include <iostream>
-#include <map>
-#include <type_traits>
+#include <mutex>
+#include <thread>
 
 using integer_t = boost::multiprecision::cpp_int;
 
 integer_t genRand2(const integer_t& a, const integer_t& b)
 {
     using namespace boost::random;
-    static random_device                rd;
-    static mt19937                      gen{rd()};
+    const std::thread::id       this_id = std::this_thread::get_id();
+    static random_device        rd;
+    const auto                  sed = rd() + std::hash<std::thread::id>{}(this_id);
+    static thread_local mt19937 gen{sed};
     uniform_int_distribution<integer_t> dist{a, b};
     return dist(gen);
+}
+
+integer_t parallelGenRandPrime(const integer_t& a, const integer_t& b)
+{
+    if(b - a <= 2 || a >= b)
+        return 2;
+    integer_t                globalRet;
+    std::atomic_bool         found{false};
+    std::mutex               resultMutex;
+    std::vector<std::thread> threads;
+    const std::size_t        size = std::thread::hardware_concurrency() + 1;
+    threads.reserve(size);
+    const auto threadFunc = [&a, &b, &found, &globalRet, &resultMutex] {
+        integer_t localRet;
+        do
+        {
+            localRet = genRand2(a, b);
+            // make it odd
+            localRet |= 1;
+            constexpr int millerAttempts = 25;
+            while(!found && localRet <= b
+                && !boost::multiprecision::miller_rabin_test(localRet, millerAttempts))
+            {
+                localRet += 2;
+            }
+        } while(!found && localRet > b);
+        if(!found)
+        {
+            found = true;
+            std::lock_guard lock{resultMutex};
+            globalRet = localRet;
+        }
+    };
+    std::generate_n(std::back_inserter(threads), size,
+        [&threadFunc] { return std::thread{threadFunc}; });
+    for(auto& t : threads)
+        if(t.joinable())
+            t.join();
+    return globalRet;
 }
 
 int main(const int argc, const char** argv)
@@ -35,32 +77,11 @@ try
         nDitits = 1024;
     }
 
-    using boost::multiprecision::pow;
-    const integer_t a = pow(integer_t{radix}, static_cast<unsigned>(nDitits - 1));
-    const integer_t b = pow(integer_t{radix}, static_cast<unsigned>(nDitits)) - 1;
-    if(b - a <= 2)
-    {
-        std::cout << 2 << std::endl;
-        return 0;
-    }
-
-    integer_t randNum;
-    int       nAttempts = 0;
-    do
-    {
-        randNum = genRand2(a, b);
-        // make it odd
-        randNum |= 1;
-
-        constexpr int millerAttempts = 25;
-        while(randNum <= b
-            && !boost::multiprecision::miller_rabin_test(randNum, millerAttempts))
-        {
-            randNum += 2;
-            ++nAttempts;
-        }
-    } while(randNum > b);
-    std::cout << randNum << std::endl;
+    const integer_t a =
+        boost::multiprecision::pow(integer_t{radix}, static_cast<unsigned>(nDitits - 1));
+    const integer_t b =
+        boost::multiprecision::pow(integer_t{radix}, static_cast<unsigned>(nDitits)) - 1;
+    std::cout << parallelGenRandPrime(a, b) << std::endl;
     return 0;
 }
 catch(const std::exception& ex)
