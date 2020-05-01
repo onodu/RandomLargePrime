c9d696b0ee4cedd9268ee4759ec98668a408df94
Ivan Bogdanov <c3t3o7@gmail.com>
2020-05-01 23:40:00 +0400

    Use boost::scoped_thread

--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
 #include <algorithm>
-#include <atomic>
+#include <boost/atomic/atomic.hpp>
+#include <boost/chrono.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/miller_rabin.hpp>
-#include <boost/random.hpp>
+#include <boost/random/mersenne_twister.hpp>
 #include <boost/random/random_device.hpp>
+#include <boost/random/seed_seq.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/scoped_thread.hpp>
 #include <functional>
 #include <iostream>
-#include <mutex>
-#include <thread>
 
 using integer_t = boost::multiprecision::cpp_int;
 
+boost::mutex uniquePrintMutex;
+template<class... Args>
+void uniquePrintln(std::ostream& os, const Args&... args)
+{
+    boost::lock_guard lock{uniquePrintMutex};
+    (os << ... << args);
+    os << std::endl;
+}
+
+static boost::atomic<unsigned> sed = 0;
 integer_t genRand2(const integer_t& a, const integer_t& b)
 {
-    using namespace boost::random;
-    const thread_local std::thread::id this_id = std::this_thread::get_id();
-    static random_device               rd;
-    static const auto                  rdSed = rd();
-    const auto           sed = rdSed + std::hash<std::thread::id>{}(this_id);
-    thread_local mt19937 gen{sed};
-    uniform_int_distribution<integer_t> dist{a, b};
+    const auto getSedseq = [] {
+        using value_t = boost::random::mt19937::result_type;
+        static const auto rdSed = static_cast<value_t>(boost::random::random_device{}());
+        const auto chronoSed = static_cast<value_t>(
+            boost::chrono::system_clock::now().time_since_epoch().count());
+        const auto tidSed = static_cast<value_t>(
+            boost::hash<boost::thread::id>{}(boost::this_thread::get_id()));
+        return boost::random::seed_seq{rdSed, chronoSed, tidSed};
+    };
+    static thread_local boost::random::mt19937 gen{sed++};
+    boost::random::uniform_int_distribution<integer_t> dist{a, b};
     return dist(gen);
 }
 
@@ -27,61 +44,76 @@ integer_t parallelGenRandPrime(const integer_t& a, const integer_t& b)
 {
     if(b - a <= 2 || a >= b)
         return 2;
-    integer_t                globalRet;
-    std::atomic_bool         found{false};
-    std::mutex               resultMutex;
-    std::vector<std::thread> threads;
-    const std::size_t        size = std::thread::hardware_concurrency() + 1;
-    threads.reserve(size);
-    const auto threadFunc = [&a, &b, &found, &globalRet, &resultMutex] {
-        integer_t localRet;
-        do
-        {
-            localRet = genRand2(a, b);
-            // make it odd
-            localRet |= 1;
-            constexpr int millerAttempts = 25;
-            while(!found && localRet <= b
-                && !boost::multiprecision::miller_rabin_test(localRet, millerAttempts))
+    integer_t globalRet;
+
+    {
+        boost::atomic_bool found{false};
+        boost::mutex resultMutex;
+        using jthread_t = boost::scoped_thread<boost::join_if_joinable>;
+        std::vector<jthread_t> threads;
+        // const std::size_t size = 1;
+        const std::size_t size = boost::thread::hardware_concurrency() + 1;
+        threads.reserve(size);
+        const auto threadFunc = [&a, &b, &found, &globalRet, &resultMutex] {
+            integer_t localRet;
+            do
             {
-                localRet += 2;
+                localRet = genRand2(a, b);
+                // make it odd
+                localRet |= 1;
+                constexpr int millerAttempts = 25;
+                while(!found && localRet <= b
+                    && !boost::multiprecision::miller_rabin_test(
+                        localRet, millerAttempts))
+                {
+                    localRet += 2;
+                }
+            } while(!found && localRet > b);
+            if(!found)
+            {
+                boost::lock_guard lock{resultMutex};
+                if(!found)
+                {
+                    found = true;
+                    globalRet = std::move(localRet);
+                }
             }
-        } while(!found && localRet > b);
-        if(!found)
-        {
-            found = true;
-            std::lock_guard lock{resultMutex};
-            globalRet = localRet;
-        }
-    };
-    std::generate_n(std::back_inserter(threads), size,
-        [&threadFunc] { return std::thread{threadFunc}; });
-    for(auto& t : threads)
-        if(t.joinable())
-            t.join();
+        };
+        std::generate_n(std::back_inserter(threads), size,
+            [&threadFunc] { return jthread_t{threadFunc}; });
+    }
     return globalRet;
+}
+
+auto measureGenRandPrime(const integer_t& a, const integer_t& b)
+{
+    const auto t1 = boost::chrono::steady_clock::now();
+    std::cout << parallelGenRandPrime(a, b) << std::endl;
+    const auto t2 = boost::chrono::steady_clock::now();
+    return boost::chrono::duration<double>{t2 - t1};
 }
 
 int main(const int argc, const char** argv)
 try
 {
     int radix = 0;
-    int nDitits = 0;
+    int nDigits = 0;
     if(argc > 2)
     {
         radix = std::stoi(argv[1]);
-        nDitits = std::stoi(argv[2]);
+        nDigits = std::stoi(argv[2]);
     }
-    if(radix <= 0 || nDitits <= 0)
+    if(radix <= 0 || nDigits <= 0)
     {
         radix = 2;
-        nDitits = 1024;
+        nDigits = 1024;
     }
 
     const integer_t a =
-        boost::multiprecision::pow(integer_t{radix}, static_cast<unsigned>(nDitits - 1));
+        boost::multiprecision::pow(integer_t{radix}, static_cast<unsigned>(nDigits - 1));
     const integer_t b =
-        boost::multiprecision::pow(integer_t{radix}, static_cast<unsigned>(nDitits)) - 1;
+        boost::multiprecision::pow(integer_t{radix}, static_cast<unsigned>(nDigits)) - 1;
+
     std::cout << parallelGenRandPrime(a, b) << std::endl;
     return 0;
 }
